821f8061dbb529339b19f7a31eeafa620d84014d
Botelho23 <filipinho5313@gmail.com>
2026-01-15 13:14:12 -0300

    Primeiro commit - calculadora em Python

---
+++ CALCULADORA.py
@@ -0,0 +1,34 @@
+print("=== CALCULADORA SIMPLES ===")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print("\nEscolha a operação:")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+opcao = input("Digite a opção (1/2/3/4): ")
+
+if opcao == "1":
+    resultado = num1 + num2
+    print("Resultado:", resultado)
+
+elif opcao == "2":
+    resultado = num1 - num2
+    print("Resultado:", resultado)
+
+elif opcao == "3":
+    resultado = num1 * num2
+    print("Resultado:", resultado)
+
+elif opcao == "4":
+    if num2 != 0:
+        resultado = num1 / num2
+        print("Resultado:", resultado)
+    else:
+        print("Erro: divisão por zero!")
+
+else:
+    print("Opção inválida!")
